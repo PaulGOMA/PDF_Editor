fe51a451e8c3e9f567f1a3dab950774cd7cf5059
PaulGOMA <paulgoma07@gmail.com>
2025-01-07 16:38:02 +0100

    fix bugs

--- canvas.py
+++ canvas.py
@@ -1,7 +1,8 @@
 import sys
-from PySide6.QtWidgets import QApplication, QWidget, QMainWindow, QGraphicsScene, QGraphicsView, QGraphicsRectItem, QGraphicsPathItem
+from PySide6.QtWidgets import QApplication, QWidget, QMainWindow, QGraphicsScene, QGraphicsView, QGraphicsRectItem, QGraphicsPathItem, QGraphicsItem
 from PySide6.QtCore import QSize, QRectF, QPoint, Qt
 from PySide6.QtGui import QColor, QPainter, QPen, QBrush, QPainterPath
+from pymupdf import Rect
 
 from extractor import Extractor
 
@@ -18,6 +19,34 @@ def centerWindow(window: QWidget) -> None:
     y = (getResolutions().height() - window.height()) // 2
 
     window.move(x, y)
+
+
+class CustomGraphicsItem(QGraphicsItem):
+    def __init__(self, bounding_rect: Rect, type_shape: QBrush | QPen | tuple, path: QPainterPath | QRectF):
+        super().__init__()
+        self.bounding_rect = bounding_rect
+        self.path = path
+        self.type_shape = type_shape
+
+    def boundingRect(self) -> QRectF:
+        bounding = QRectF(QPoint(self.bounding_rect.x0, self.bounding_rect.y0), QPoint(self.bounding_rect.x1, self.bounding_rect.y1))
+        return bounding
+    
+    def paint(self, painter: QPainter, option, widget):
+        if isinstance(self.type_shape, QBrush):
+            painter.setBrush(self.type_shape)
+            painter.setPen(self.type_shape.color())
+            painter.drawRect(self.path) if isinstance(self.path, QRectF) else painter.drawPath(self.path)
+        
+        elif isinstance(self.type_shape, QPen):
+            painter.setPen(self.type_shape)
+            painter.drawRect(self.path) if isinstance(self.path, QRectF) else painter.drawPath(self.path)
+
+        elif isinstance(self.type_shape, tuple):
+            painter.setBrush(self.type_shape[0])
+            painter.setPen(self.type_shape[1])
+            painter.drawRect(self.path) if isinstance(self.path, QRectF) else painter.drawPath(self.path)
+        
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -48,162 +77,6 @@ class MainWindow(QMainWindow):
 
         self.draw()
 
-    # def draw(self):
-    #     graphic_list = self.r.extract_graphics()
-
-    #     for graphic in graphic_list:
-
-    #         if graphic["type"] == "s":
-    #             color = graphic["color"]
-    #             stroke_opacity = graphic["stroke_opacity"]
-    #             width = graphic["width"]
-    #             pen = QPen(QColor(int(color[0] * 255), int(color[1] * 255), int(color[2] * 255)))
-    #             pen.setWidth(width)
-    #         elif graphic["type"] == "f":
-    #             fill = graphic["fill"]
-    #             fill_opacity = graphic["fill_opacity"]
-    #             brush = QBrush(QColor(int(fill[0] * 255), int(fill[1] * 255), int(fill[2] * 255)))
-    #         elif graphic["type"] == "fs":
-    #             color = graphic["color"]
-    #             stroke_opacity = graphic["stroke_opacity"]
-    #             width = graphic["width"]
-    #             pen = QPen(QColor(int(color[0] * 255), int(color[1] * 255), int(color[2] * 255)))
-    #             pen.setWidth(width)
-    #             fill = graphic["fill"]
-    #             fill_opacity = graphic["fill_opacity"]
-    #             brush = QBrush(QColor(int(fill[0] * 255), int(fill[1] * 255), int(fill[2] * 255)))
-
-    #         for item in graphic["items"]:
-    #             if item[0] == 're':
-    #                 rect = QRectF(QPoint(item[1].x0, item[1].y0), QPoint(item[1].x1, item[1].y1))
-    #                 drawing = QGraphicsRectItem(rect) 
-    #                 drawing.setBrush(brush)
-    #                 if fill_opacity is not None:
-    #                     drawing.setOpacity(fill_opacity)
-    #                 elif stroke_opacity is not None:
-    #                     drawing.setOpacity(stroke_opacity)
-    #                 self.scene.addItem(drawing)  
-    #             else:
-    #                 path = QPainterPath()
-    #                 start_point = item[1]
-    #                 path.moveTo(start_point.x, start_point.y)
-    #                 if item[0] == 'l':
-    #                     point = item[2]
-    #                     path.lineTo(point.x, point.y)
-
-    #                 elif item[0] == 'c':
-    #                     p2 = item[2]
-    #                     p3 = item[3]
-    #                     p4 = item[4]
-    #                     path.cubicTo(QPoint(p2.x, p2.y), QPoint(p3.x, p3.y), QPoint(p4.x, p4.y))
-
-    #                 if graphic["even_odd"]: 
-    #                     path.setFillRule(Qt.WindingFill) 
-    #                 else: 
-    #                     path.setFillRule(Qt.OddEvenFill) 
-            
-    #                 drawing = QGraphicsPathItem(path)
-    #                 drawing.setPen(pen)
-    #                 if fill_opacity is not None:
-    #                     drawing.setOpacity(fill_opacity)
-    #                 elif stroke_opacity is not None:
-    #                     drawing.setOpacity(stroke_opacity)
-    #                 self.scene.addItem(drawing)
-
-    #                 rect = QRectF(QPoint(graphic['rect'].x0, graphic['rect'].y0), QPoint(graphic['rect'].x1, graphic['rect'].y1))
-    #                 draw_rect = QGraphicsRectItem(rect)
-    #                 draw_rect.setBrush(brush)
-    #                 if fill_opacity is not None:
-    #                     draw_rect.setOpacity(fill_opacity)
-    #                 elif stroke_opacity is not None:
-    #                     draw_rect.setOpacity(stroke_opacity)
-    #                 self.scene.addItem(draw_rect)
-
-
-    # def draw(self):
-    #     graphic_list = self.r.extract_graphics()
-
-    #     for graphic in graphic_list:
-    #         fill = graphic['fill']
-    #         color = graphic['color']
-    #         fill_opacity = graphic['fill_opacity']
-    #         stroke_opacity = graphic['stroke_opacity']
-
-    #         for item in graphic['items']:
-    #             if item[0] == 're':
-    #                 rect = QRectF(QPoint(item[1].x0, item[1].y0), QPoint(item[1].x1, item[1].y1))
-    #                 drawing = QGraphicsRectItem(rect)
-    #                 if fill is None and color is not None:
-    #                     pen = QPen(QColor(color[0] * 255, color[1] * 255, color[2] * 255))
-    #                     pen.setWidth(graphic['width'])
-    #                     drawing.setPen(pen)
-    #                 elif fill is not None and color is None:
-    #                     brush = QBrush(QColor(fill[0] * 255, fill[1] * 255, fill[2] * 255))
-    #                     pen = QPen(QColor(fill[0] * 255, fill[1] * 255, fill[2] * 255))
-    #                     pen.setWidth(1)
-    #                     drawing.setBrush(brush)
-    #                     drawing.setPen(pen)
-    #                 elif fill is not None and color is not None:
-    #                     pen = QPen(QColor(color[0] * 255, color[1] * 255, color[2] * 255))
-    #                     pen.setWidth(graphic['width'])
-    #                     brush = QBrush(QColor(fill[0] * 255, fill[1] * 255, fill[2] * 255))
-    #                     drawing.setBrush(brush)
-    #                 else:
-    #                     print("impossible")
-
-    #                 if fill_opacity is not None:
-    #                     drawing.setOpacity(fill_opacity)
-    #                 elif stroke_opacity is not None:
-    #                     drawing.setOpacity(stroke_opacity)
-
-    #                 self.scene.addItem(drawing)
-    #             else:
-
-    #                 rect = QRectF(QPoint(graphic['rect'].x0, graphic['rect'].y0), QPoint(graphic['rect'].x1, graphic['rect'].y1))
-    #                 draw_rect = QGraphicsRectItem(rect)
-    #                 self.scene.addItem(draw_rect)
-
-    #                 path = QPainterPath()
-    #                 start_point = item[1]
-    #                 path.moveTo(start_point.x, start_point.y)
-    #                 if item[0] == 'l':
-    #                     point = item[2]
-    #                     path.lineTo(point.x, point.y)
-    #                 elif item[0] == 'c':
-    #                     p2 = item[2]
-    #                     p3 = item[3]
-    #                     p4 = item[4]
-    #                     path.cubicTo(QPoint(p2.x, p2.y), QPoint(p3.x, p3.y), QPoint(p4.x, p4.y))
-    #                 drawing = QGraphicsPathItem(path)
-
-                    
-
-    #                 if fill is None and color is not None:
-    #                     pen = QPen(QColor(color[0] * 255, color[1] * 255, color[2] * 255))
-    #                     pen.setWidth(graphic['width'])
-    #                     drawing.setPen(pen)
-    #                 elif fill is not None and color is None:
-    #                     brush = QBrush(QColor(fill[0] * 255, fill[1] * 255, fill[2] * 255))
-    #                     pen = QPen(QColor(fill[0] * 255, fill[1] * 255, fill[2] * 255))
-    #                     pen.setWidth(1)
-    #                     drawing.setBrush(brush)
-    #                     drawing.setPen(pen)
-    #                 elif fill is not None and color is not None:
-    #                     pen = QPen(QColor(color[0] * 255, color[1] * 255, color[2] * 255))
-    #                     pen.setWidth(graphic['width'])
-    #                     drawing.setPen(pen)
-    #                     brush = QBrush(QColor(fill[0] * 255, fill[1] * 255, fill[2] * 255))
-    #                     draw_rect.setBrush(brush)
-    #                 else:
-    #                     print("impossible")
-
-    #                 if fill_opacity is not None:
-    #                     drawing.setOpacity(fill_opacity)
-    #                 elif stroke_opacity is not None:
-    #                     drawing.setOpacity(stroke_opacity)
-
-    #                 self.scene.addItem(drawing)
-
 
     def type_draw(self, type: str, fill: tuple | None, color: tuple | None, width: float | None) -> QBrush | QPen | tuple:
         if type == 'f':
@@ -220,6 +93,37 @@ class MainWindow(QMainWindow):
             return brush, pen
 
 
+    def get_paths(self, paths_list: list, even_odd: bool | None) -> QRectF | QPainterPath:
+
+        if paths_list[0][0] == "re":
+            item = paths_list[0][1]
+            rect = QRectF(QPoint(item.x0, item.y0), QPoint(item.x1, item.y1))
+            return rect
+        
+        else:
+            path = QPainterPath()
+
+            for item in paths_list:
+
+                p1 = item[1]
+                path.moveTo(p1.x, p1.y)
+
+                if item[0] == 'l':
+                    p2 = item[2]
+                    path.lineTo(p2.x, p2.y)
+                elif item[0] == 'c':
+                    p2 = item[2]
+                    p3 = item[3]
+                    p4 = item[4]
+                    path.cubicTo(QPoint(p2.x, p2.y), QPoint(p3.x, p3.y), QPoint(p4.x, p4.y))
+
+
+            if even_odd is not None:
+                path.setFillRule(Qt.OddEvenFill) if even_odd else path.setFillRule(Qt.WindingFill)
+
+        return path
+
+
     def draw(self):
         graphic_list = self.r.extract_graphics()
 
@@ -231,119 +135,62 @@ class MainWindow(QMainWindow):
             stroke_opacity = graphic['stroke_opacity']
             even_odd = graphic["even_odd"]
 
-            # if graphic['type'] == 'f':        
-            #     brush = QBrush(QColor(int(fill[0] * 255), int(fill[1] * 255), int(fill[2] * 255)))
-            # elif graphic['type'] == 's':
-            #     pen = QPen(QColor(int(color[0] * 255), int(color[1] * 255), int(color[2] * 255)))               
-            #     pen.setWidth(int(width))
-            # elif graphic['type'] == 'fs':
-            #     brush = QBrush(QColor(int(fill[0] * 255), int(fill[1] * 255), int(fill[2] * 255)))
-            #     pen = QPen(QColor(int(color[0] * 255), int(color[1] * 255), int(color[2] * 255)))
-            #     pen.setWidth(int(width))
+            path = self.get_paths(graphic['items'], even_odd)
+            result = self.type_draw(graphic['type'], fill, color, width)
 
-            for item in graphic['items']:
-                if item[0] == 're':
-                    rect = QRectF(QPoint(item[1].x0, item[1].y0), QPoint(item[1].x1, item[1].y1))
-                    drawing = QGraphicsRectItem(rect)
-                    result = self.type_draw(graphic['type'], fill, color, width)
-                    if isinstance(result, QBrush):
-                        drawing.setBrush(result)
-                        drawing.setPen(result.color())
-                    elif isinstance(result, QPen):
-                        drawing.setPen(result)
-                    else:
-                        drawing.setBrush(result[0])
-                        drawing.setPen(result[1])
-                        
-                    if fill_opacity is not None:
-                        drawing.setOpacity(fill_opacity)
-                    elif stroke_opacity is not None:
-                        drawing.setOpacity(stroke_opacity)
-
-                    self.scene.addItem(drawing)
-
-                else:
-                    path = QPainterPath()
-                    p1 = item[1]
-                    path.moveTo(p1.x, p1.y)
-
-                    if item[0] == 'l':
-                        p2 = item[2]
-                        path.lineTo(p2.x, p2.y)
-                    elif item[0] == 'c':
-                        p2 = item[2]
-                        p3 = item[3]
-                        p4 = item[4]
-                        path.cubicTo(QPoint(p2.x, p2.y), QPoint(p3.x, p3.y), QPoint(p4.x, p4.y))
-
-                    if even_odd is not None:
-                        path.setFillRule(Qt.OddEvenFill) if even_odd else path.setFillRule(Qt.WindingFill)
-
-                    drawing = QGraphicsPathItem(path)
-                    
-                    result = self.type_draw(graphic['type'], fill, color, width)
-
-                    if isinstance(result, QBrush): 
-                        drawing.setBrush(result)
-                    elif isinstance(result, QPen):                    
-                        if graphic['lineCap'] is not None:
-                            line_cap = int(max(graphic['lineCap'])) if isinstance(graphic['lineCap'], tuple) else int(graphic['lineCap'])
-                            if line_cap == 1:
-                                result.setCapStyle(Qt.RoundCap)
-                            elif line_cap == 2:
-                                result.setCapStyle(Qt.SquareCap)
-                            else:
-                                result.setCapStyle(Qt.FlatCap)
-
-                        if graphic['lineJoin'] is not None:
-                            line_join = int(max(graphic['lineJoin'])) if isinstance(graphic['lineJoin'], tuple) else int(graphic['lineJoin'])
-                            if line_join == 1:
-                                result.setJoinStyle(Qt.RoundJoin)
-                            elif line_join == 2:
-                                result.setJoinStyle(Qt.MiterJoin)
-                            else:
-                                result.setJoinStyle(Qt.BevelJoin)
-
-                        drawing.setPen(result)
-                    else:
-                        # rect = QRectF(QPoint(graphic['rect'].x0, graphic['rect'].y0), QPoint(graphic['rect'].x1, graphic['rect'].y1))
-                        # draw_rect = QGraphicsRectItem(rect)
-                        # draw_rect.setBrush(result[0])
-    
-                        # if fill_opacity is not None:
-                        #     draw_rect.setOpacity(fill_opacity)
-
-                        # self.scene.addItem(draw_rect)
-                        if graphic['lineCap'] is not None:
-                            line_cap = int(max(graphic['lineCap'])) if isinstance(graphic['lineCap'], tuple) else int(graphic['lineCap'])
-                            if line_cap == 1:
-                                result[1].setCapStyle(Qt.RoundCap)
-                            elif line_cap == 2:
-                                result[1].setCapStyle(Qt.SquareCap)
-                            else:
-                                result[1].setCapStyle(Qt.FlatCap)
-
-                        if graphic['lineJoin'] is not None:
-                            line_join = int(max(graphic['lineJoin'])) if isinstance(graphic['lineJoin'], tuple) else int(graphic['lineJoin'])
-                            if line_join == 1:
-                                result[1].setJoinStyle(Qt.RoundJoin)
-                            elif line_join == 2:
-                                result[1].setJoinStyle(Qt.MiterJoin)
-                            else:
-                                result[1].setJoinStyle(Qt.BevelJoin)
-                        drawing.setPen(result[1])
-                        drawing.setBrush(result[0])
-
-                    if fill_opacity is not None:
-                        drawing.setOpacity(fill_opacity)
-                    elif stroke_opacity is not None:
-                        drawing.setOpacity(stroke_opacity)
-
-                    self.scene.addItem(drawing)
-                    
-
+            if isinstance(result, QBrush):
+                drawing = CustomGraphicsItem(bounding_rect=graphic['rect'], type_shape=result, path=path)
             
+            elif isinstance(result, QPen):                    
+                if graphic['lineCap'] is not None:
+                    line_cap = int(max(graphic['lineCap'])) if isinstance(graphic['lineCap'], tuple) else int(graphic['lineCap'])
+                    if line_cap == 1:
+                        result.setCapStyle(Qt.RoundCap)
+                    elif line_cap == 2:
+                        result.setCapStyle(Qt.SquareCap)
+                    else:
+                        result.setCapStyle(Qt.FlatCap)
 
+                if graphic['lineJoin'] is not None:
+                    line_join = int(max(graphic['lineJoin'])) if isinstance(graphic['lineJoin'], tuple) else int(graphic['lineJoin'])
+                    if line_join == 1:
+                        result.setJoinStyle(Qt.RoundJoin)
+                    elif line_join == 2:
+                        result.setJoinStyle(Qt.MiterJoin)
+                    else:
+                        result.setJoinStyle(Qt.BevelJoin)
+
+                drawing = CustomGraphicsItem(bounding_rect=graphic['rect'], type_shape=result, path=path)
+
+            elif isinstance(result, tuple): 
+                if graphic['lineCap'] is not None:
+                    line_cap = int(max(graphic['lineCap'])) if isinstance(graphic['lineCap'], tuple) else int(graphic['lineCap'])
+                    if line_cap == 1:
+                        result[1].setCapStyle(Qt.RoundCap)
+                    elif line_cap == 2:
+                        result[1].setCapStyle(Qt.SquareCap)
+                    else:
+                        result[1].setCapStyle(Qt.FlatCap)
+
+                if graphic['lineJoin'] is not None:
+                    line_join = int(max(graphic['lineJoin'])) if isinstance(graphic['lineJoin'], tuple) else int(graphic['lineJoin'])
+                    if line_join == 1:
+                        result[1].setJoinStyle(Qt.RoundJoin)
+                    elif line_join == 2:
+                        result[1].setJoinStyle(Qt.MiterJoin)
+                    else:
+                        result[1].setJoinStyle(Qt.BevelJoin)
+
+                drawing = CustomGraphicsItem(bounding_rect=graphic['rect'], type_shape=result, path=path)
+
+            if fill_opacity is not None:
+                drawing.setOpacity(fill_opacity)
+            elif stroke_opacity is not None:
+                drawing.setOpacity(stroke_opacity)   
+            
+            self.scene.addItem(drawing)
+
+                    
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
